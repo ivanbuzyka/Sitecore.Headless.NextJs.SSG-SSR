417df39208c19dc1dedc8e9c882f1ca868eb3eea
Ivan Buzyka <ivan.buzyka@sitecore.com>
2023-03-07 12:26:28 +0100

    modified ContentBlock component to use component-level data fetching: call a dummy API and render the response json

--- src/components/ContentBlock.tsx
+++ src/components/ContentBlock.tsx
@@ -1,4 +1,12 @@
-import { Text, RichText, Field, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
+import {
+  Text,
+  RichText,
+  Field,
+  withDatasourceCheck,
+  GetStaticComponentProps,
+  GetServerSideComponentProps,
+  useComponentProps,
+} from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
 
 type ContentBlockProps = ComponentProps & {
@@ -13,12 +21,41 @@ type ContentBlockProps = ComponentProps & {
  * This is the most basic building block of a content site, and the most basic
  * JSS component that's useful.
  */
-const ContentBlock = ({ fields }: ContentBlockProps): JSX.Element => (
-  <div className="contentBlock">
-    <Text tag="h2" className="contentTitle" field={fields.heading} />
+const ContentBlock = ({ fields, rendering }: ContentBlockProps): JSX.Element => {
+  const thirdPartyData = rendering.uid ? useComponentProps<string>(rendering.uid) : undefined;
+  return (
+    <div className="contentBlock">
+      <Text tag="h2" className="contentTitle" field={fields.heading} />
 
-    <RichText className="contentDescription" field={fields.content} />
-  </div>
-);
+      <RichText className="contentDescription" field={fields.content} />
+      <div>
+        <h4>Raw JSON of all the beers:</h4>
+        <pre>{JSON.stringify(thirdPartyData, null, 2)}</pre>
+      </div>
+    </div>
+  );
+};
+
+//our custom method to fetch the data from third-party
+export const fetchBeerData = (searchWord: string): Promise<string> =>
+  fetch('https://api.sampleapis.com/beers/' + searchWord).then((response) => response.json());
+
+//getStaticProps method will be used when component is located on the page that uses SSG
+export const getStaticProps: GetStaticComponentProps = async (rendering) => {
+  //following line can be used to pass parameters to the third-party call, for example from field values
+  const someDataFromRenderingFields = rendering?.fields?.heading;
+  const customData = await fetchBeerData('ale');
+
+  return customData;
+};
+
+//getServerSideProps method will be used when component is located on the page that uses SSR
+export const getServerSideProps: GetServerSideComponentProps = async (rendering, layoutData) => {
+  //following line can be used to pass parameters to the third-party call, for example from field values
+  const someDataFromRenderingFields = rendering?.fields?.heading;
+  const customData = await fetchBeerData('ale');
+
+  return customData;
+};
 
 export default withDatasourceCheck()<ContentBlockProps>(ContentBlock);
